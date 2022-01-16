3da4b2d977ac08951814eb49a5b7f62d159739f7
Tayo Akosile <oluwatayoakosile@gmail.com>
2022-01-16 21:49:39 +0100

    Create:Started Iwello Ng New Homepage

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Login from "../components/Login";
+
+const index = () => {
+  return (
+    <div>
+      Hello World
+      {/* Spotify Logo */}
+      <Login />
+      {/* Login with spotify button */}
+    </div>
+  );
+};
+
+export default index;
